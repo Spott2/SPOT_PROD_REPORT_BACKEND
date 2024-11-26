a04b093f71665b2c994a1134bad694c12846e2b1
Spott2 <145750822+Spott2@users.noreply.github.com>
2024-11-26 16:06:28 +0530

    changes

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
-import { TransactionQr, Qr, Station, LoginSession} from '@spot-demo/shared-entities';
+import { TransactionQr, Qr, Station, LoginSession, Equipment} from '@spot-demo/shared-entities';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TransactionQr, Qr, Station, LoginSession])],
+  imports: [TypeOrmModule.forFeature([TransactionQr, Qr, Station, LoginSession, Equipment])],
   controllers: [ReportsController],
   providers: [ReportsService],
 })
